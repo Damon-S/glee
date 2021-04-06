db3633fb79fb48f5d154eddf1befef345e56cfe6
Damon <rasseyaniy85@gmail.com>
2021-04-06 09:40:02 +0300

    added product of week

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-
+    var mixer = mixitup('.product__categories');
+    $('.top-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+    })
 });
